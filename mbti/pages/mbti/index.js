cc4f1061fff49f1c37d185fb0bbc838f6dd00127
qbnyp <moneycloudy@naver.com>
2024-03-13 00:54:07 +0900

    코드 설명, 주석달기

--- mbti/pages/mbti/index.js
+++ mbti/pages/mbti/index.js
@@ -1,34 +1,39 @@
 import { useEffect, useState } from "react";
-import { Mbti_Layout,StartLogo,StartPageLayout,StartButton,StartLogo1,StartLogo2 } from "../../styles/mbti_css";
+import { Mbti_Layout,StartLogo,StartPageLayout,StartButton,StartLogo_Mbti,StartLogo_motion } from "../../styles/mbti_css";
 import theme from "../../styles/theme";
 
 export default function App() {
     
 const setVh = () => {
-    const vh = window.innerHeight * 0.01;
+    const vh = window.innerHeight * 0.01; 
+    //현재 viewport 파악후 1/100 하기
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    //element 최상위 루트 요소의 스타일 '--vh'라는 변수 추가후 {vh}px 값 저장하기
 }
-    useEffect(()=> {
-        setVh()
-    
+    useEffect(()=> {  //useEffect는 화면이 렌더링되는 순간 실행
+        setVh() //화면이 켜지는순간 함수실행 height 스타일 변경
 
-    function onResize() {
-        setVh()
-    }
+        function onResize() { //렌더링이 아니여도 창크기 변경시 대응하기 위한 함수생성
+            setVh() 
+        }
+        
+        window.addEventListener('resize' , onResize) 
+        //브라우저 창크기가 변경될때마다 호출되는 함수
 
-    window.addEventListener('resize' , onResize)
-
-    },[])
+    } , [])
 
     const [page,setPage] = useState(0)
 
     return (
-        <Mbti_Layout>
-            {page===0?
+        <Mbti_Layout>    
+            {page===0? 
+            //삼항연산자를 사용하여 0일때 StartPageLayout까지 보여줌
+            //버튼이 눌려질때까지 0페이지를 보여주고 1이 넘어가면 다음페이지를 보여줌!
+            //삼항연산자를 이렇게도 사용할수 있다.
             <StartPageLayout>
                 <StartLogo>
-                    <StartLogo1>MBTI</StartLogo1>
-                    <StartLogo2>▼</StartLogo2>
+                    <StartLogo_Mbti>MBTI</StartLogo_Mbti>
+                    <StartLogo_motion>▼</StartLogo_motion>
                 </StartLogo>
                 <StartButton onClick={()=> setPage(1)}>테스트 시작하기</StartButton>
             </StartPageLayout>
--- mbti/styles/mbti_css.js
+++ mbti/styles/mbti_css.js
@@ -5,10 +5,12 @@ import styled from "@emotion/styled";
 export const Mbti_Layout = styled.div`
     width : 100vw;
     font-size : 14px;
-    display :flex;
+    display : flex;
     justify-content: center;
     background-color: yellow;
     height:calc(var(--vh,1vh)*100);
+    //layout의 높이를 다양한 화면기기 뷰포트에 알맞게끔 스타일 설정하기
+    //var(변수값1 ,변수값2 ) 변수값 1이없으면 2로 설정하기
 `
 
 export const StartPageLayout = styled.div`
@@ -30,7 +32,7 @@ export const StartLogo = styled.div`
     font-size: 25px;
     font-weight:700;
 `
-export const StartLogo1 = styled.div`
+export const StartLogo_Mbti = styled.div`
     background-color : #413730;
     color: #ffea35;
     width : 100px;
@@ -41,7 +43,7 @@ export const StartLogo1 = styled.div`
     border-radius : 50px;
 `
 
-export const StartLogo2 = styled.div`
+export const StartLogo_motion = styled.div`
     margin-top : 15px;
 `
 
