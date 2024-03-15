7ded91f9bcb594a207fc8cea206ef6ca2b3e8aee
qbnyp <moneycloudy@naver.com>
2024-03-15 16:29:09 +0900

    깃허브 페이지 배포

--- mbti/next.config.mjs
+++ mbti/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "jeongho77-mbti-page";
+
 const nextConfig = {
+
+  output: 'export',
   reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
 export default nextConfig;
