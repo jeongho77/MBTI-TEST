ac93b856c6b8523a31206f1c199d2f539ee176dc
qbnyp <moneycloudy@naver.com>
2024-03-15 16:54:57 +0900

    배포왜안돼

--- mbti/next.config.mjs
+++ mbti/next.config.mjs
@@ -6,8 +6,9 @@ const nextConfig = {
 
   output: 'export',
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  assetPrefix: !debug ? `${repository}` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  basePath: `${repository}`,
 };
 
 export const prefix =
